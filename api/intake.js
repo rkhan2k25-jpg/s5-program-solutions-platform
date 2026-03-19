6d2314ff95c3cbd8b5c3d9e57918610cd7044a4c
rkhan2k25-jpg <rkhan2k25@gmail.com>
2026-03-19 14:52:20 -0400

    Create intake.js

---
+++ api/intake.js
@@ -0,0 +1,6 @@
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    return res.status(200).json({ message: "API is working" });
+  }
+  return res.status(405).json({ message: "Method not allowed" });
+}
